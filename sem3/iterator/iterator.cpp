376cecba36a0f71a11aac01938f1e3d1a516c169
Jahor Laryn <mrojahor@gmail.com>
2021-09-07 21:38:13 +0300

    Add iterator scratch.

---
+++ sem3/iterator/iterator.cpp
@@ -0,0 +1,19 @@
+// Итератор
+// - итератор по массиву
+// - итератор по map<string, ...> 
+// возвращающий ключ-значение с определённой 
+// подстрокой в ключе
+#include <map>
+#include <string>
+#include <iostream>
+#include <utility>
+#define Map map
+
+using std::cout;
+using std::pair;
+using std::map;
+using std::string;
+
+int main(){
+    return 0;
+}
